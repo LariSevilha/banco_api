9c649d3dcc5cf45fffbc144416b70f5b146d545b
larissa sevilha <sevilha.larissa10@gmail.com>
2025-05-28 08:53:09 -0400

    Rename conta_bancaria table to conta_bancarias and update related references; modify Docker volume path

--- db/migrate/20250526193630_create_conta_bancaria.rb
+++ db/migrate/20250526193630_create_conta_bancaria.rb
@@ -1,6 +1,6 @@
 class CreateContaBancaria < ActiveRecord::Migration[8.0]
   def change
-    create_table :conta_bancaria do |t|
+    create_table :conta_bancarias do |t|
       t.references :user, null: false, foreign_key: true
       t.string :numero_conta
       t.string :agencia
@@ -8,6 +8,6 @@ class CreateContaBancaria < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
-    add_index :conta_bancaria, :numero_conta, unique: true
+    add_index :conta_bancarias, :numero_conta, unique: true
   end
 end
